320ee4ed419e23d4b269a61dfb95c62671b2b86b
santiagoBassi <santiagobassi21@gmail.com>
2025-03-29 18:18:14 -0300

    firebase auth working

--- prognosia-frontend/app/index.tsx
+++ prognosia-frontend/app/index.tsx
@@ -1,15 +1,7 @@
-import { Redirect } from 'expo-router';
-import { Button, Text } from 'react-native';
 import LoginScreen from './(auth)/login';
-import RegisterScreen from './(auth)/register'
-import { signUp, signIn} from './../services/auth';
 
 export default function App() {
   return (
-    // <RegisterScreen />
-    
-    <Button onPress={() => {signUp("santiagobassi21@gmail.com", "12345678")}} title="Sign Up" />
-    
-
+    <LoginScreen />
   );
 }
