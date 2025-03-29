5c2aa54ffcc25cd1701739e719a30682d5d3fbc8
conradohillar <hillarconrado@gmail.com>
2025-03-29 00:42:50 -0300

    Fixed React project to use JS instead of TS

---
+++ prognosia-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>My React App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
